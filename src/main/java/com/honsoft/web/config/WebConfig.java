12b17e036bb218eb05cd6143965360f4617abeba
changnam go <changnma.go@gmail.com>
2021-05-12 01:34:46 +0900

    add json, xml converter in pom.xml and configure disable accept header

--- src/main/java/com/honsoft/web/config/WebConfig.java
+++ src/main/java/com/honsoft/web/config/WebConfig.java
@@ -2,6 +2,8 @@ package com.honsoft.web.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,4 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.jsp("/WEB-INF/jsp/", ".jsp");
 	}
 
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(true).ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML).mediaType("json", MediaType.APPLICATION_JSON);
+	}
 }
